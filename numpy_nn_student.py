94927b626d733e5675e5e04dcf0a761c5e3f6cc8
madiagne <mame-absa.diagne@etudiant.univ-rennes.fr>
2024-11-06 16:59:21 +0100

    until task 10

--- numpy_nn_student.py
+++ numpy_nn_student.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-@authors: XXX and YYY
+@authors: DIAGNE Mame Absa and Giacometti Luca
 """
 
 import struct
@@ -67,9 +67,10 @@ def preprocess_input(input) :
 # Task 5-6: creating and initializing matrices of weights
 
 def layer_weights(m , n) :
-    return np.random.standard_normal(size = m * n) / np.sqrt(n)
+    return np.random.standard_normal(size = (m , n)) / np.sqrt(n)
 
 w1 = layer_weights(784 , 128)
+#print(w1.shape)
 w2 = layer_weights(128 , 64)
 w3 = layer_weights(64 , 10)
         
@@ -87,8 +88,54 @@ def sigmoid_prime(x) :
     return np.exp(-x) / (np.exp(-x) + 1 ) ** 2
         
 # Task 8-9: forward pass
-        
+
+def forward_pass(input) :
+    z1 = np.dot(input , w1)
+    a1 = sigmoid(z1)
+    z2 = np.dot(a1 , w2)
+    a2 = sigmoid(z2)
+    z3 = np.dot(a2 , w3)
+    return [sigmoid(z3)]
+
+def forward_pass_v2(input) :
+    z1 = np.dot(input , w1)
+    a1 = sigmoid(z1)
+    z2 = np.dot(a1 , w2)
+    a2 = sigmoid(z2)
+    z3 = np.dot(a2 , w3)
+    return [(input, a1, a2, sigmoid(z3)) , (z1, z2)]
+
+#flattening the training data
+testprep = preprocess_input(x_train)
+print(testprep, testprep.shape, testprep.ndim)
+
+input = testprep[0] #testing on the first image (first row)
+print(input, input.shape)
+res = forward_pass_v2(input)
+print("showing res")
+#print(res)
+
 # Task 10: backpropagation
+
+def backpropagation (forward_res, expected_output) :
+    e3 = forward_res[0][-1] - expected_output
+
+    deltaw3 = np.outer(np.transpose(forward_res[0][-2]) , e3)
+
+    e2 = np.dot(e3 , np.transpose(w3)) * sigmoid_prime(forward_res[1][1])
+
+    deltaw2 = np.outer(np.transpose(forward_res[0][-3]) , e2)
+
+    e1 = np.dot(e2 , np.transpose(w2)) * sigmoid_prime(forward_res[1][0])
+
+    deltaw1 = np.outer(np.transpose(forward_res[0][0]) , e1)
+
+    return [deltaw1, deltaw2, deltaw3]
+
+test10  = backpropagation(forward_pass_v2(input), ([0,0,0,0,0,0,0,1,0,0]))
+print("showing what is inside test 10")
+print(test10[0].shape, len(test10))
+print(test10)
         
 # Task 11: weight updates
         
